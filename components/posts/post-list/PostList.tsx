8a9f8604836c7856f8d6b745758e391bbfb52615
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-08-07 19:29:51 +0200

    Displaying postlist

--- components/posts/post-list/PostList.tsx
+++ components/posts/post-list/PostList.tsx
@@ -1,6 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import PostListElement from "../post-list-element/PostListElement";
+import { Post } from "../post-list-element/PostListElement.types";
 import styles from "./PostList.module.scss";
+import { PostListProps } from "./PostList.types";
 
-export default function PostList() {
-  return <div>PostList</div>;
+const SAMPLE_POSTS = [
+  {
+    author: "Randall G. Holcombe",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/06/menger_kata_dem2.png",
+    title: "A demokrácia és a szabadság bonyolult viszonya (2. rész)",
+    excerpt:
+      " Az amerikai demokrácia felépítése A huszonegyedik századi amerikaiak demokráciának tekintik a kormányzatukat, és emiatt a demokrácia kritikája Amerika-ellenesnek tűnik számukra. Mégis, az USA alkotmányának vizsgálata azt mutatja, hogy az amerikai alapítók olyan kormányzatot terveztek, amely szándékosan el volt szigetelve a közvéleménytől. A Függetlenségi Nyilatkozat a szabadságot elidegeníthetetlen jognak nyilvánítja, ami",
+  },
+  {
+    author: "Randall G. Holcombe",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/05/menger_kata_dem1.png",
+    title: "A demokrácia és a szabadság bonyolult viszonya (1. rész)",
+    excerpt:
+      "A fordító előszava: Randall G. Holcombe ezt a tanulmányt 2021 elején jelentette meg. Egy év múltán különös aktualitást nyert Magyarországon, ahol zsinórban negyedszer nyert alkotmányozó többséget a jelenlegi kormánypárt az országgyűlési választásokon, és ezt sokan „erős demokratikus felhatalmazásként” értékelik. A hidegháború korszakában – az 1950-es évektől az 1980-as évekig – globális ideológiai szakadék volt a",
+  },
+  {
+    author: "Tóth István",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/05/menger_kalkul_ci_s.png",
+    title: "A kalkulációs vita. Darabok egy kirakóshoz",
+    excerpt:
+      "Kornai János emlékére Miként köztudott, Karl Marx és Friedrich Engels, a tudományos szocializmus német megalapítói nem dolgoztak ki a kommunizmus gazdasági berendezkedésére vonatkozóan működési elveket és módszereket; könyvespolcot betöltő prózai munkásságuk nélkülözi az általuk áhított szebb jövő valamennyire is konkrét leírását. Marx egyáltalán nem kívánt „recepteket írni a jövendő lacikonyhája számára”, miként azt A tőke",
+  },
+  {
+    author: "Randall G. Holcombe",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/06/menger_kata_dem2.png",
+    title: "A demokrácia és a szabadság bonyolult viszonya (2. rész)",
+    excerpt:
+      " Az amerikai demokrácia felépítése A huszonegyedik századi amerikaiak demokráciának tekintik a kormányzatukat, és emiatt a demokrácia kritikája Amerika-ellenesnek tűnik számukra. Mégis, az USA alkotmányának vizsgálata azt mutatja, hogy az amerikai alapítók olyan kormányzatot terveztek, amely szándékosan el volt szigetelve a közvéleménytől. A Függetlenségi Nyilatkozat a szabadságot elidegeníthetetlen jognak nyilvánítja, ami",
+  },
+  {
+    author: "Randall G. Holcombe",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/05/menger_kata_dem1.png",
+    title: "A demokrácia és a szabadság bonyolult viszonya (1. rész)",
+    excerpt:
+      "A fordító előszava: Randall G. Holcombe ezt a tanulmányt 2021 elején jelentette meg. Egy év múltán különös aktualitást nyert Magyarországon, ahol zsinórban negyedszer nyert alkotmányozó többséget a jelenlegi kormánypárt az országgyűlési választásokon, és ezt sokan „erős demokratikus felhatalmazásként” értékelik. A hidegháború korszakában – az 1950-es évektől az 1980-as évekig – globális ideológiai szakadék volt a",
+  },
+  {
+    author: "Tóth István",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/05/menger_kalkul_ci_s.png",
+    title: "A kalkulációs vita. Darabok egy kirakóshoz",
+    excerpt:
+      "Kornai János emlékére Miként köztudott, Karl Marx és Friedrich Engels, a tudományos szocializmus német megalapítói nem dolgoztak ki a kommunizmus gazdasági berendezkedésére vonatkozóan működési elveket és módszereket; könyvespolcot betöltő prózai munkásságuk nélkülözi az általuk áhított szebb jövő valamennyire is konkrét leírását. Marx egyáltalán nem kívánt „recepteket írni a jövendő lacikonyhája számára”, miként azt A tőke",
+  },
+  {
+    author: "Randall G. Holcombe",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/06/menger_kata_dem2.png",
+    title: "A demokrácia és a szabadság bonyolult viszonya (2. rész)",
+    excerpt:
+      " Az amerikai demokrácia felépítése A huszonegyedik századi amerikaiak demokráciának tekintik a kormányzatukat, és emiatt a demokrácia kritikája Amerika-ellenesnek tűnik számukra. Mégis, az USA alkotmányának vizsgálata azt mutatja, hogy az amerikai alapítók olyan kormányzatot terveztek, amely szándékosan el volt szigetelve a közvéleménytől. A Függetlenségi Nyilatkozat a szabadságot elidegeníthetetlen jognak nyilvánítja, ami",
+  },
+  {
+    author: "Randall G. Holcombe",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/05/menger_kata_dem1.png",
+    title: "A demokrácia és a szabadság bonyolult viszonya (1. rész)",
+    excerpt:
+      "A fordító előszava: Randall G. Holcombe ezt a tanulmányt 2021 elején jelentette meg. Egy év múltán különös aktualitást nyert Magyarországon, ahol zsinórban negyedszer nyert alkotmányozó többséget a jelenlegi kormánypárt az országgyűlési választásokon, és ezt sokan „erős demokratikus felhatalmazásként” értékelik. A hidegháború korszakában – az 1950-es évektől az 1980-as évekig – globális ideológiai szakadék volt a",
+  },
+  {
+    author: "Tóth István",
+    image:
+      "https://mengerblogcom.files.wordpress.com/2022/05/menger_kalkul_ci_s.png",
+    title: "A kalkulációs vita. Darabok egy kirakóshoz",
+    excerpt:
+      "Kornai János emlékére Miként köztudott, Karl Marx és Friedrich Engels, a tudományos szocializmus német megalapítói nem dolgoztak ki a kommunizmus gazdasági berendezkedésére vonatkozóan működési elveket és módszereket; könyvespolcot betöltő prózai munkásságuk nélkülözi az általuk áhított szebb jövő valamennyire is konkrét leírását. Marx egyáltalán nem kívánt „recepteket írni a jövendő lacikonyhája számára”, miként azt A tőke",
+  },
+];
+
+export default function PostList({ displayImages }: PostListProps) {
+  const [posts, setPosts] = useState<Array<Post>>(SAMPLE_POSTS);
+
+  return (
+    <>
+      {posts.map((post, index) => {
+        return (
+          <PostListElement
+            post={post}
+            displayImages={displayImages}
+            key={index}
+          />
+        );
+      })}
+    </>
+  );
 }

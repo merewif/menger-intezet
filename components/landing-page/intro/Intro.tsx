9a0c7a39f873994c403dd1367859d7d779ead48c
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-08-07 19:30:06 +0200

    Adding intro component

--- components/landing-page/intro/Intro.tsx
+++ components/landing-page/intro/Intro.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
-import styles from "./Intro.module.scss"
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import styles from "./Intro.module.scss";
 
 export default function Intro() {
   return (
-    <div>Intro</div>
-  )
+    <div className={styles.container}>
+      <img src="/assets/images/menger.png" alt="Carl Menger" />
+      <div className={styles.textContainer}>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          natus aspernatur quam voluptatibus, mollitia ad sequi culpa! Sed,
+          voluptate esse.
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Necessitatibus ut tenetur sint maiores voluptatem. Iure dolore
+          molestias reiciendis? Enim sapiente soluta, adipisci, obcaecati, ipsum
+          fuga praesentium quis officiis sequi eos minus alias repellendus
+          accusamus eligendi? Perferendis quidem dicta maxime iure, nobis
+          voluptatum ad ullam aliquid voluptas quas odit, reiciendis assumenda
+          recusandae. Ullam corrupti reiciendis quos rem dolores quis eveniet
+          enim veritatis nesciunt vero itaque ea repellendus, doloremque
+          consequatur, harum, expedita dignissimos qui officiis minus iste
+          autem?
+        </p>
+      </div>
+    </div>
+  );
 }

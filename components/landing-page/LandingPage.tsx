87de9e4b1d11c46cc46a1130df72f0ed5201b6eb
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-08-07 19:30:33 +0200

    Importint components to landing page

--- components/landing-page/LandingPage.tsx
+++ components/landing-page/LandingPage.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import PostList from '../posts/post-list/PostList'
+import FeaturedPosts from './featured-posts/FeaturedPosts'
+import Intro from './intro/Intro'
 import styles from "./LandingPage.module.scss"
 
 export default function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div className={styles.landingPage}>
+      <Intro />
+      <FeaturedPosts />
+      <PostList displayImages={false} />
+    </div>
   )
 }

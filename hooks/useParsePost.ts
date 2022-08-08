61668aa72b37245158e2418029808a6f566d9342
balint.taborszki <taborszki@jegymester.hu>
2022-08-08 16:10:21 +0200

    Fetching posts by slug

--- hooks/useParsePost.ts
+++ hooks/useParsePost.ts
@@ -3,26 +3,43 @@ import { Post } from "../types/PostResponse";
 import sanitizeHtml from "sanitize-html";
 
 const useParsePost = (postID: string | number) => {
-  const [postData, setPostData] = useState<Post>()
+  const [postData, setPostData] = useState<Post>();
   const [author, setAuthor] = useState<string>("");
   const [title, setTitle] = useState<string>("");
   const [excerpt, setExcerpt] = useState<string>("");
-  const [content, setContent] = useState<string>('')
-  const [image, setImage] = useState<string>('')
-  
+  const [content, setContent] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+
   useEffect(() => {
-    fetchPost();
+    console.log(postID);
+    const postIDisString: boolean = postID?.toString().includes("-");
+    if (!postIDisString) {
+      fetchPostByID();
+    }
+    if (postIDisString) {
+      fetchPostBySlug();
+    }
   }, [postID]);
 
-
-  async function fetchPost() {
+  async function fetchPostByID() {
     await fetch(`https://public-api.wordpress.com/wp/v2/sites/mengerblog.com/posts/${postID}`)
       .then((res) => res.json())
       .then((data: Post) => {
         if (data.title) {
-        setPostData(data);
-        parsePost(data);
-      }
+          setPostData(data);
+          parsePost(data);
+        }
+      });
+  }
+
+  async function fetchPostBySlug() {
+    await fetch(`https://public-api.wordpress.com/wp/v2/sites/mengerblog.com/posts?slug=${postID}`)
+      .then((res) => res.json())
+      .then((data: Array<Post>) => {
+        if (data[0].title) {
+          setPostData(data[0]);
+          parsePost(data[0]);
+        }
       });
   }
 
@@ -46,8 +63,8 @@ const useParsePost = (postID: string | number) => {
 export default useParsePost;
 
 export interface ParsedPosts {
-    title: string,
-    author: string,
-    excerpt: string,
-    image: string,
+  title: string;
+  author: string;
+  excerpt: string;
+  image: string;
 }

90b0074293027f20bb761d4efbc99eeeb8b0b113
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-08-09 00:33:34 +0200

    Embedding video

--- pages/videok.tsx
+++ pages/videok.tsx
@@ -1,10 +1,21 @@
+/* eslint-disable react/no-unknown-property */
 import React from "react";
 import Layout from "../components/Layout";
 
 export default function Videos() {
   return (
     <Layout>
-      <div>videok</div>
+      <div>
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/videoseries?list=UUR9ZpmR-YA-XRjhx2NRTJVw"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </Layout>
   );
 }

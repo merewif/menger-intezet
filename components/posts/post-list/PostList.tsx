2a4fbd05f4939a3123b9048abff600d4a4affd70
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-08-08 08:28:03 +0200

    Removing the first three element from postlist

--- components/posts/post-list/PostList.tsx
+++ components/posts/post-list/PostList.tsx
@@ -3,13 +3,15 @@ import { PostsContext } from "../../../pages/_app";
 import PostListElement from "../post-list-element/PostListElement";
 import { Post } from "../../../types/PostResponse";
 import { PostListProps } from "./PostList.types";
+import * as _ from "lodash"
 
 export default function PostList({ displayImages }: PostListProps) {
   const posts = useContext(PostsContext);
+  const postsWithoutTheFirstThree: Array<Post> = _.drop(posts, 3);
 
   return (
     <>
-      {posts.map((post: Post, index: number) => {
+      {postsWithoutTheFirstThree.map((post: Post, index: number) => {
         return (
           <PostListElement
             post={post}

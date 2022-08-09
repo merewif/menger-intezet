62a9788c045163d7096baf50ad83292dc7bee3b9
balint.taborszki <taborszki@jegymester.hu>
2022-08-09 14:51:03 +0200

    Initializing getStaticProps implementation

--- pages/posts/[postID].tsx
+++ pages/posts/[postID].tsx
@@ -7,13 +7,13 @@ import useParsePost from "../../hooks/useParsePost";
 import styles from "../../styles/Post.module.scss";
 import parse from "html-react-parser";
 import LoadingBackdrop from "../../components/LoadingBackdrop";
+import { GetStaticPropsContext } from "next";
+import { Post } from "../../types/PostResponse";
 
 export default function SinglePost() {
   const router = useRouter();
   const { postID } = router.query;
-  const { author, title, content, image, loading } = useParsePost(
-    Array.isArray(postID) ? postID![0] : postID!
-  );
+  const { author, title, content, image, loading } = useParsePost(Array.isArray(postID) ? postID![0] : postID!);
   const pageTitle = `${title} | ${author}`;
   
   useEffect(() => {
@@ -47,3 +47,37 @@ export default function SinglePost() {
     </>
   );
 }
+
+// export async function getStaticProps(context: GetStaticPropsContext) {
+//   let numberOfApiPages = 0;
+//   let page = 0;
+//   let posts: Array<Post> = [];
+//   fetch(`https://public-api.wordpress.com/wp/v2/sites/mengerblog.com/posts?per_page=$100&page=${page + 1}`)
+//     .then((response) => {
+//       for (let header of response.headers.entries()) {
+//         if (header[0] === "x-wp-totalpages") {
+//           numberOfApiPages = parseInt(header[1]);
+//         }
+//     }
+//   });
+
+//   for (let i = 0; i > page; i++) {
+//     fetchPostsByPage(i)
+//       .then((data: Array<Post>) => posts = [...posts, ...data]);
+//   }
+
+//   return {
+//     props: {
+//       posts
+//     },
+//   }
+// }
+
+// async function fetchPostsByPage(page: number) {
+//   let fetchedPosts: Array<Post> = [];
+//   await fetch(`https://public-api.wordpress.com/wp/v2/sites/mengerblog.com/posts?per_page=$100&page=${page + 1}`)
+//     .then((response) => response.json())
+//     .then(data => fetchedPosts = data);
+
+//   return fetchedPosts;
+// }

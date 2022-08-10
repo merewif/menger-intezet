40d2cf224e32b1f85814e3d45974021371d34390
balint.taborszki <taborszki@jegymester.hu>
2022-08-10 16:39:04 +0200

    Adding on-demand revalidation

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   }
 
   try {
+    res.send('Trying to revalidate, please do not close this window else the whole universe will collapse unto itself.');
     await res.revalidate("/posts/randall-g-holcombe-a-demokracia-es-a-szabadsag-bonyolult-viszonya-2-resz");
     await res.revalidate("/bejegyzesek/1");
     return res.json({ revalidated: true });

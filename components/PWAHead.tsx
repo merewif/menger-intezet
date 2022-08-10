4c220648280b168bb2be34dfc0d7c729fd22d97c
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-08-10 02:22:42 +0200

    Fixing pwa head

--- components/PWAHead.tsx
+++ components/PWAHead.tsx
@@ -21,9 +21,9 @@ export default function PWAHead() {
 
       <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
       <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-      <link rel='manifest' href='/manifest.json' />
       <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
       <link rel='shortcut icon' href='/favicon.ico' />
+
       <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
           
       <meta name='twitter:card' content='summary' />
@@ -31,18 +31,16 @@ export default function PWAHead() {
       <meta name='twitter:title' content='Menger Intézet' />
       <meta name='twitter:description' content='Menger Intézet' />
       {/* <meta name='twitter:image' content='https://yourdomain.com/icons/android-chrome-192x192.png' /> */}
-      <meta name='twitter:creator' content='@DavidWShadow' />
+      <meta name='twitter:creator' content='@ValPasch' />
+
       <meta property='og:type' content='website' />
       <meta property='og:title' content='Menger Intézet' />
       <meta property='og:description' content='Menger Intézet' />
       <meta property='og:site_name' content='Menger Intézet' />
-      <meta property='og:url' content='https://yourdomain.com' />
+      <meta property='og:url' content='https://menger.vercel.app' />
       {/* <meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' /> */}
 
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700;900&family=Oswald:wght@200;300;400;500;600;700&display=swap"
-      />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700;900&family=Oswald:wght@200;300;400;500;600;700&display=swap" />
     </Head>
   );
 }

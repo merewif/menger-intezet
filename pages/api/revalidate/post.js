e2b8e8d8f1bb53ea065b5677c91d6281eacd3968
balint.taborszki <taborszki@jegymester.hu>
2022-08-10 16:56:28 +0200

    Seperating revalidations into seperate api endpoints

--- pages/api/revalidate/post.js
+++ pages/api/revalidate/post.js
@@ -4,11 +4,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    // res.send('Trying to revalidate, please do not close this window else the whole universe will collapse unto itself.');
     await res.revalidate("/posts/randall-g-holcombe-a-demokracia-es-a-szabadsag-bonyolult-viszonya-2-resz");
-    // console.log('Posts revalidated')
-    // await res.revalidate("/bejegyzesek/1");
-    // console.log('PostPa')
     return res.json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error revalidating");

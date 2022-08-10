538205369ac8ea33f3cd4f6253cf96da76d6f120
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-08-11 00:53:42 +0200

    Adjusting logo

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -9,14 +9,16 @@ export default function Logo() {
       <Link href={"/"}>
         <a>
           <Image
+          className={styles.logoImage}
             src={"/assets/images/logo.png"}
             width={300}
-            height={100}
+            height={35}
             objectFit={"contain"}
             alt={"Carl Menger Intézet"}
           />
         </a>
       </Link>
+      <div className={styles.secondLine}>intézet</div>
     </div>
   );
 }

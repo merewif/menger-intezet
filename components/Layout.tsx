33aea9891e6b73ff85f249ac4fbbb46ef6ee9457
balint.taborszki <taborszki@jegymester.hu>
2022-08-12 17:02:16 +0200

    Adding swipe open feature

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,10 +4,37 @@ import MenuDrawer from "./menu-drawer/MenuDrawer";
 import Menu from "./menu/Menu";
 
 export default function Layout({ children }: { children: any }) {
+  const [touchStart, setTouchStart] = useState<number | null>(null);
+  const [touchEnd, setTouchEnd] = useState<number | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (event: any) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(event.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (event: any) => setTouchEnd(event.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isRightSwipe) {
+      setOpen(true);
+    }
+  };
   return (
     <>
-      <div className={styles.bodyContainerMobile}>
-        <MenuDrawer /> {children}
+      <div
+        className={styles.bodyContainerMobile}
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}>
+        <MenuDrawer open={open} setOpen={setOpen} /> {children}
       </div>
       <div className={styles.bodyContainerDesktop}>
         <Menu showLogo={true} /> {children}
--- components/menu-drawer/MenuDrawer.tsx
+++ components/menu-drawer/MenuDrawer.tsx
@@ -1,21 +1,16 @@
-import React, { useState } from "react";
+import React, { SetStateAction, useState } from "react";
 import Drawer from "@mui/material/Drawer";
 import MenuIcon from "@mui/icons-material/Menu";
 import Menu from "../menu/Menu";
 import styles from "./MenuDrawer.module.scss";
 import Logo from "../logo/Logo";
 
-export default function MenuDrawer() {
-  const [open, setOpen] = useState<boolean>(false);
-
+export default function MenuDrawer({ open, setOpen }: { open: boolean; setOpen: Function }) {
   return (
     <>
       <div className={styles.menuDrawerContainer}>
         <Logo />
-        <MenuIcon
-          onClick={() => setOpen((open) => !open)}
-          sx={{ cursor: "pointer" }}
-        />
+        <MenuIcon onClick={() => setOpen((open: boolean) => !open)} sx={{ cursor: "pointer" }} />
         <Drawer anchor={"right"} open={open} onClose={() => setOpen(false)}>
           <Menu showLogo={false} />
         </Drawer>

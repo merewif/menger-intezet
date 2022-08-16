3c21fe5ec5003138b75690e6c2dd60e5a7ae27a0
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-08-16 18:47:18 +0200

    Refactoring to remove code duplication

--- pages/cikksorozatok.tsx
+++ pages/cikksorozatok.tsx
@@ -2,7 +2,7 @@ import { NextSeo } from "next-seo";
 import Link from "next/link";
 import React from "react";
 import Layout from "../components/Layout";
-import { getAllTags, getPostBySearchQuery } from "../helpers/getPosts";
+import { getAllTags, getFilteredPostData, getPostBySearchQuery } from "../helpers/getPosts";
 import styles from "../styles/Collections.module.scss";
 import {
   Collection,
@@ -163,26 +163,11 @@ export async function getStaticProps() {
     };
 
     for (const article of collection.articles) {
-      await getPostBySearchQuery(article).then((post) => {
-        const filteredTagData = _.map(post.tags, (tag) => {
-          const tagData = _.filter(fetchedTags, (fetchedTag) => {
-            return fetchedTag.id == tag;
-          })[0];
-          return { id: tagData.id, name: tagData.name, slug: tagData.slug };
-        });
-        let filteredArticleData: FilteredPost = {
-          id: post.id,
-          slug: post.slug,
-          title: post.title,
-          content: post.content,
-          excerpt: post.excerpt,
-          jetpack_featured_media_url: post.jetpack_featured_media_url,
-          tags: filteredTagData,
-        };
-        collectionWithData.articles.push({
-          title: article,
-          data: filteredArticleData,
-        });
+      const postData = await getPostBySearchQuery(article);
+      const filteredPostData = await getFilteredPostData([postData]);
+      collectionWithData.articles.push({
+        title: article,
+        data: filteredPostData[0],
       });
     }
 

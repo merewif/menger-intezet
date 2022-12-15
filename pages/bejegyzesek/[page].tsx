23f8e64f02524376ef2f31a21fa809de6b1f60e3
Bálint Táborszki <taborszkib@gmail.com>
2022-12-15 14:23:39 +0100

    Add ISR

--- pages/bejegyzesek/[page].tsx
+++ pages/bejegyzesek/[page].tsx
@@ -1,21 +1,21 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
-import { Pagination } from "@mui/material";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import Layout from "../../components/Layout";
-import styles from "../../styles/Posts.module.scss";
-import { Post } from "../../types/PostResponse";
-import LoadingBackdrop from "../../components/LoadingBackdrop";
-import { NextSeo } from "next-seo";
-import { getAllPosts, getAllTags, getFilteredPostData } from "../../helpers/getPosts";
-import * as _ from "lodash";
-import { PostsProps } from "../../types/PostList";
-import PostGrid from "../../components/posts/post-grid/PostGrid";
+import {Pagination} from '@mui/material';
+import {useRouter} from 'next/router';
+import React, {useState} from 'react';
+import Layout from '../../components/Layout';
+import styles from '../../styles/Posts.module.scss';
+import {Post} from '../../types/PostResponse';
+import LoadingBackdrop from '../../components/LoadingBackdrop';
+import {NextSeo} from 'next-seo';
+import {getAllPosts, getAllTags, getFilteredPostData} from '../../helpers/getPosts';
+import * as _ from 'lodash';
+import {PostsProps} from '../../types/PostList';
+import PostGrid from '../../components/posts/post-grid/PostGrid';
 
 const POSTS_PER_PAGE = 9;
 
-export default function Posts({ posts, pageCount, page }: PostsProps) {
+export default function Posts({posts, pageCount, page}: PostsProps) {
   const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
 
@@ -30,17 +30,17 @@ export default function Posts({ posts, pageCount, page }: PostsProps) {
   return (
     <>
       <NextSeo
-        title={"Bejegyzések | Menger Intézet"}
+        title={'Bejegyzések | Menger Intézet'}
         openGraph={{
           url: `https://menger.hu/bejegyzesek/${page}`,
-          title: "Bejegyzések | Menger Intézet",
-          type: "article",
+          title: 'Bejegyzések | Menger Intézet',
+          type: 'article',
           images: [
             {
-              url: "/assets/images/fb-featured.png",
+              url: '/assets/images/fb-featured.png',
               width: 1200,
               height: 630,
-              type: "image/png",
+              type: 'image/png',
             },
           ],
         }}
@@ -64,7 +64,7 @@ export default function Posts({ posts, pageCount, page }: PostsProps) {
   );
 }
 
-export async function getStaticProps({ params }: { params: { page: string } }) {
+export async function getStaticProps({params}: {params: {page: string}}) {
   const posts: Array<Post> = await getAllPosts();
   const filteredPosts = await getFilteredPostData(posts);
   const postChunks = _.chunk(filteredPosts, POSTS_PER_PAGE);
@@ -74,14 +74,14 @@ export async function getStaticProps({ params }: { params: { page: string } }) {
     page: parseInt(params.page),
   };
 
-  return { props: props };
+  return {props: props, revalidate: 60};
 }
 
 export async function getStaticPaths() {
   const posts: Array<Post> = await getAllPosts();
   const postChunks = _.chunk(posts, POSTS_PER_PAGE);
   const paths = _.map(postChunks, (chunk, index) => {
-    return { params: { page: (index + 1).toString() } };
+    return {params: {page: (index + 1).toString()}};
   });
 
   return {
